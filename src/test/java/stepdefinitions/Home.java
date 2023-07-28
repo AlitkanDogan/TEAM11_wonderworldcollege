37b1e2663df3564906d4374632e2fb22e26167c4
AlitkanDogan <dgmaltkn@gmail.com>
2023-07-28 06:21:51 +0300

    dogan28070621

--- src/test/java/stepdefinitions/Home.java
+++ src/test/java/stepdefinitions/Home.java
@@ -2,19 +2,26 @@ package stepdefinitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import pages.HomePage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class Home {
 
-    HomePage homePage=new HomePage();
+    HomePage homePage = new HomePage();
 
-    WebDriver driver= Driver.getDriver();
+    WebDriver driver = Driver.getDriver();
 
-//VVVVVVVVVVVVVVVV  US01 VVVVVVVVVVVVVVVVVVV
+   Actions actions=new Actions(driver);
+
+    //VVVVVVVVVVVVVVVV  US01 VVVVVVVVVVVVVVVVVVV
     @Given("The user goes to the {string} address")
     public void the_user_goes_to_the_address(String url) {
         driver.get(ConfigReader.getProperty(url));
@@ -24,6 +31,7 @@ public class Home {
     public void it_verifies_that_the_is_visible_on_the_homepage() {
         Assert.assertTrue(homePage.logoCollege_header.isDisplayed());
     }
+
     @And("User closes the page")
     public void userClosesThePage() {
         Driver.quitDriver();
@@ -32,9 +40,32 @@ public class Home {
     //========================US02=============================
 
     @Given("The user verifies that the {string} is visible in the navbar section")
-    public void the_user_verifies_that_the_is_visible_in_the_navbar_section(String string) {
-        
+    public void the_user_verifies_that_the_is_visible_in_the_navbar_section(String mail) {
+        String expectedMailAddress = ConfigReader.getProperty(mail);
+        String actualMailAdress = homePage.mailInfo_header.getText();
+        Assert.assertEquals(expectedMailAddress, actualMailAdress);
+    }
 
+    @When("The user goes to the homepage footer section")
+    public void the_user_goes_to_the_homepage_footer_section() {
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        ReusableMethods.bekle(2);
+
+    }
+
+    @When("The user verifies that the school's phone number information is visible in the footer section")
+    public void the_user_verifies_that_the_school_s_phone_number_information_is_visible_in_the_footer_section() {
+        Assert.assertTrue(homePage.contactLink_footer.isDisplayed());
+    }
+
+    @Then("The user verifies that the school's email address information is visible in the footer section")
+    public void the_user_verifies_that_the_school_s_email_address_information_is_visible_in_the_footer_section() {
+        Assert.assertTrue(homePage.emailUsLink_footer.isDisplayed());
+    }
+
+    @Then("The user verifies that the school's school address information is visible in the footer section")
+    public void the_user_verifies_that_the_school_s_school_address_information_is_visible_in_the_footer_section() {
+        Assert.assertTrue(homePage.addressLink_footer.isDisplayed());
     }
 
 }
--- src/test/resources/features/US_013.feature
+++ src/test/resources/features/US_013.feature
@@ -1,11 +1,22 @@
 @US13
-  Feature: As a user, I want the contact information of the school to be available on the homepage for easy access.
+Feature: As a user, I want the contact information of the school to be available on the homepage for easy access.
 
 
-    @TC1_US13
-    Scenario: On the homepage navbar, it should be verified that the school's email address is visible.
+  @TC1_US13
+  Scenario: On the homepage navbar, it should be verified that the school's email address is visible.
 
     Given The user goes to the "url" address
-    Given The user verifies that the 'college mail address' is visible in the navbar section
+    Given The user verifies that the "collegeMailAddress" is visible in the navbar section
     Given User closes the page
+
+  @TC2_US13
+  Scenario: In the footer of the homepage, it should be verified that the school's phone number, email address, and school address information are visible.
+
+
+    Given The user goes to the "url" address
+    When The user goes to the homepage footer section
+    And The user verifies that the school's phone number information is visible in the footer section
+    Then The user verifies that the school's email address information is visible in the footer section
+    And The user verifies that the school's school address information is visible in the footer section
+    And User closes the page
 

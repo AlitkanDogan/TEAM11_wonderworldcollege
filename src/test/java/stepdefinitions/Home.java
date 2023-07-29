fd64823e3b5e2a042df9930ef61a67ba4159c068
DoganAlitkan <dgnaltkn@gmail.com>
2023-07-30 02:18:13 +0300

    v

--- src/test/java/stepdefinitions/Home.java
+++ src/test/java/stepdefinitions/Home.java
@@ -68,4 +68,15 @@ public class Home {
         Assert.assertTrue(homePage.addressLink_footer.isDisplayed());
     }
 
+
+    @Then("The user, on the homepage top bar, it  verify that the school's phone information is visible as Call Us")
+    public void theUserOnTheHomepageTopBarItVerifyThatTheSchoolSPhoneInformationIsVisibleAsCallUs() {
+        String expectedResult="Call Us";
+        String actualResult=homePage.callUs.getText();
+        System.out.println(actualResult);
+        Assert.assertTrue(actualResult.contains(expectedResult));
+
+    }
+
+    //================================================================================================
 }

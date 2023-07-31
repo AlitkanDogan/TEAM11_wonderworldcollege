7540f50038fc67bd771bf3a9ec05927954b33357
DoganAlitkan <dgnaltkn@gmail.com>
2023-07-31 14:14:47 +0300

    1414

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@US22",
+        tags = "@TC1_US22",
 
         dryRun = false
 )
